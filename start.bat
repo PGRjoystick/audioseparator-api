71e3884b29bf0e499ac649153915f596e6dac2d4
PGRjoystick <30430815+PGRjoystick@users.noreply.github.com>
2024-06-23 23:10:33 +0700

    re-arrange dependency

--- start.bat
+++ start.bat
@@ -2,8 +2,8 @@
 python -m venv venv
 call .\venv\Scripts\activate
 pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
-pip install onnxruntime-gpu
 pip install onnxruntime
+pip install onnxruntime-gpu
 pip install "audio-separator[gpu]"
 pip install fastapi
 pip install python-dotenv
